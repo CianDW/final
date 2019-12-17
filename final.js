c79e41aea52f009c8cbb40a1f1fb0e6b72a7328b
Cian Windous <cdwindous@gmail.com>
2019-12-17 11:44:39 -0700

    done?

--- final.js
+++ final.js
@@ -24,30 +24,82 @@ function ready(){
         month2 = month2 * 12;
 
         console.log(day,month,year,day2,month2,year2);
-
-        let finald = (day - day2)+(month - month2)+(year-year2);
-
-        console.log(finald);
+        if (day+month+year < day2+month2+year2){
+            let finald = (day2 - day)+(month2 - month)+(year2-year);
+            console.log(finald);
         document.getElementById("Tday").innerHTML = finald;
-    }
-}
-
-function ready2(){
-
-    StMB.onclick = function(){
-                var m = document.getElementById("StM").value;
-                console.log(m);
-            }
+        }
+        if (day+month+year > day2+month2+year2){
+            let finald = (day - day2)+(month - month2)+(year-year2);
+            console.log(finald);
+        document.getElementById("Tday").innerHTML = finald;
         }
         
+
+        
     }
-
-
+    Sec.onclick = function(){
+        var m = document.getElementById("S").value;
+        console.log(m);
+        a = m / 60
+        console.log(a);
+        document.getElementById("A1").innerHTML = a + " Minute(s)";
+    }
+    Min.onclick = function(){
+        var m = document.getElementById("M").value;
+        console.log(m);
+        a = m / 60
+        console.log(a);
+        document.getElementById("A2").innerHTML = a + " Hour(s)";
+    }
+    Hour.onclick = function(){
+        var m = document.getElementById("H").value;
+        console.log(m);
+        a = m / 24
+        console.log(a);
+        document.getElementById("A3").innerHTML = a + " Day(s)";
+    }
+    Day.onclick = function(){
+        var m = document.getElementById("D").value;
+        console.log(m);
+        a = m / 365
+        console.log(a);
+        document.getElementById("A4").innerHTML = a + " Year(s)";
+    }
+    Years.onclick = function(){
+        var m = document.getElementById("Y").value;
+        console.log(m);
+        a = m * 365
+        console.log(a);
+        document.getElementById("A5").innerHTML = a + " Day(s)";
+    }
+    HourD.onclick = function(){
+        var m = document.getElementById("DH").value;
+        console.log(m);
+        a = m * 24
+        console.log(a);
+        document.getElementById("A6").innerHTML = a + " Hour(s)";
+    }
+    HourM.onclick = function(){
+        var m = document.getElementById("HM").value;
+        console.log(m);
+        a = m * 60
+        console.log(a);
+        document.getElementById("A7").innerHTML = a + " Minute(s)";
+    }
+    MinS.onclick = function(){
+        var m = document.getElementById("MS").value;
+        console.log(m);
+        a = m * 60
+        console.log(a);
+        document.getElementById("A8").innerHTML = a + " Seconds(s)";
+    }
+      
+      
 }
 
 
 
 
 
-document.addEventListener("DOMContentLoaded", ready2);
 document.addEventListener("DOMContentLoaded", ready);
